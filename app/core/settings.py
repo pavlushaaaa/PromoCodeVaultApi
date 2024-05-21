ca8b99986d7a0532bf3e72d35aede073774d406b
pavlushaaaa <pavlo.horniak@lnu.edu.ua>
2024-05-21 22:40:41 +0300

    minor updates

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
+    sqlalchemy_database_url: str
 
     class Config:
         env_file = ".env"
--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.core.settings import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
