75fbf3a2f16fb0b6cdbb3e4b3e6858ae5f5a7de5
pavlushaaaa <pavlo.horniak@lnu.edu.ua>
2024-06-14 02:35:36 +0300

    add metadata

--- app/schemas/voucher_code.py
+++ app/schemas/voucher_code.py
@@ -14,5 +14,5 @@ class VoucherCodeSchema(DiscountSchema):
     used: bool
     used_at: Optional[str] = None
     last_retrieved_at: Optional[str] = None
-    metadata: Optional[dict] = None
+    code_metadata: Optional[dict] = None
 
